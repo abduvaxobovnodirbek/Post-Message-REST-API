367b33c90a7a1f9ff7a102c3e3e9176b96965ac2
nodirbekabduvaxobov <nodirbekabduvaxobov17@gmail.com>
2021-08-29 21:43:18 +0300

    new commit

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const CONNECTION_URL = "mongodb+srv://nodirbek:Id3tvfAKfvLek63y@cluster0.z5nqa.m
 const PORT = process.env.PORT || 5000
 
 app.use(cors())
-app.use(bodyParser.json({limit:"30mb",extended:true}))
-app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(express.static(path.join("public")))
 
 // storing images to fileStorage and validating them
